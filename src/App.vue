f367c0c6c382da173b2a7b51326d7fcc2946683e
kristophesankar <arjuna.kristophe.sankar@gmail.com>
2019-02-24 18:41:51 -0400

    feat: add sidebar component skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
+    <SideBar />
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
+  import SideBar from "./components/SideBar.vue"
+  import MainSection from "./components/MainSection.vue"
+
+  export default {
+    name: 'app',
+    components: {
+      SideBar,
+      MainSection,
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import url('https://fonts.googleapis.com/css?family=Raleway');
+
+  #app {
+    font-family: 'Raleway', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="sidenav" class="sidenav">
+    <span class="title">Chess.com</span><br>
+    <span class="title-break">Front-End Challenge</span>
+    <div class="event-history">
+      <span><b>Move History</b></span><br>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SideBar',
+  data: function () {
+    return {
+      boardClicks: this.$store.getters.boardClicks
+    }
+  },
+}
+</script>
+
+<style>
+  .sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+    border: solid 4px #ccc;
+  }
+
+  .title {
+    font-weight: bold;
+    color: white;
+  }
+
+  .title-break {
+    font-weight: bold;
+    color: white;
+    padding: 5px;
+  }
+
+  .event-history {
+    margin-top: 10px;
+    background: #eee;
+    padding: 10px;
+    margin: 10px;
+  }
+
+
+</style>
