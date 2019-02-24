56ca9874acf32e9160e2684615059ce9229b024d
kristophesankar <arjuna.kristophe.sankar@gmail.com>
2019-02-24 18:48:19 -0400

    feat: add mainsection component skeleton

---
+++ src/components/MainSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-section">
+    <h1>Chessboard</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainSection',
+  }
+</script>
+
+<style>
+  .main-section {
+    margin-left: 200px;
+  }
+</style>
